a13f2e9c139c6910cbd303aeacf56dfc167e12ae
Sanchir Enkhbold <sanchir@saturn.mn>
2024-04-10 16:52:53 +0800

    add skeleton

---
+++ components/Skeleton.js
@@ -0,0 +1,8 @@
+import Skeleton from 'react-loading-skeleton'
+import 'react-loading-skeleton/dist/skeleton.css'
+
+export function SkeletonSingle(){
+    return (
+        <Skeleton className='h-8' />
+    )
+}
