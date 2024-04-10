c032afc8c184613dc599bf475e0196c81ffb08a7
Sanchir Enkhbold <sanchir@saturn.mn>
2024-04-10 17:39:56 +0800

    update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,20 @@
+import { Github } from 'lucide-react'
+
 export default function Home() {
     return (
-        <div>
-            <h1>Next.js Starter Kit by <a href='https://sanchir.dev'>Sanchir Enkhbold</a></h1>
+        <div className="h-screen flex flex-col justify-center items-center gap-6 text-zinc-100">
+            <h1 className="text-2xl md:text-4xl font-semibold">Next.js Starter Kit</h1>
+            <div className="flex gap-1">
+                <span>by</span>
+                <a href='https://sanchir.dev' className="underline">Sanchir Enkhbold</a>
+            </div>
+            <div className="bg-zinc-800 px-4 py-3 rounded-xl">
+                Start editing <code className="font-medium">app/page.js</code> and save to reload.
+            </div>
+            <a href='https://github.com/sanchir2011/nextjs-starter' className='flex gap-2 font-medium items-center' target='_blank'>
+                <Github size={20} className="text-zinc-100" /> 
+                <span>Github</span>
+            </a>
         </div>
     );
 }
