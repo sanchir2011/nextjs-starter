be6af3b2d98ae0cba86ea3501443f7fce9bd8305
Sanchir Enkhbold <sanchir@saturn.mn>
2024-04-10 16:57:01 +0800

    add not found

---
+++ app/not-found.js
@@ -0,0 +1,10 @@
+import NotFound from "@/components/NotFound"
+
+export const metadata = {
+    title: "Хуудас олдсонгүй",
+    description: "Уучлаарай, таны хайсан зүйл олдсонгүй."
+}
+
+export default function NotFoundPage() {
+    return <NotFound />
+}
