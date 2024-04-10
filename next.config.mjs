9887ea89c55eb86a4c08f56a0861311d139bb24c
Sanchir Enkhbold <sanchir@saturn.mn>
2024-04-10 16:28:45 +0800

    update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async rewrites() {
+        return [
+            {
+                source: '/v1/api/:path*',
+                destination: process.env.BACKEND_URL + '/api/:path*'
+            },
+            {
+                source: '/v1/api/auth/:path*',
+                destination: process.env.BACKEND_URL + '/auth/:path*'
+            }
+        ]
+    },
+    transpilePackages: ['lucide-react'],
+    reactStrictMode: false
+}
 
 export default nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: { },
+    plugins: [
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/typography'),
+        require('@tailwindcss/forms'),
+        require('flowbite/plugin')
+    ],
 };
