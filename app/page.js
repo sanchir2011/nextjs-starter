b53403dddd82bd950018ee6ade08d1d20ef0af85
Sanchir Enkhbold <sanchir@saturn.mn>
2024-04-10 17:40:15 +0800

    update page.js

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
                 <a href='https://sanchir.dev' className="underline">Sanchir Enkhbold</a>
             </div>
             <div className="bg-zinc-800 px-4 py-3 rounded-xl">
-                Start editing <code className="font-medium">app/page.js</code> and save to reload.
+                Start editing <code className="font-medium">app/page.js</code> and save.
             </div>
             <a href='https://github.com/sanchir2011/nextjs-starter' className='flex gap-2 font-medium items-center' target='_blank'>
                 <Github size={20} className="text-zinc-100" /> 
